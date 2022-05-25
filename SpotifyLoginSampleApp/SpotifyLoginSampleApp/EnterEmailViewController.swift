14fdb7b55fd52952c9ee46fdae21f0b9428e2ed3
LimYunHwi <88yhtserof@naver.com>
2022-05-25 14:18:44 +0900

    Firebase 연결하기 #1
    + 개인 정보 보호를 위한 GoogleService-Info 누락

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/EnterEmailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class EnterEmailViewController: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
@@ -32,10 +33,50 @@ class EnterEmailViewController: UIViewController {
     }
     
     @IBAction func tapNextButton(_ sender: UIButton) {
-        let main = UIStoryboard(name: "Main", bundle: nil)
+        //Firebase 이메일/비밀번호 인증
+        let email = emailTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+        //신규 사용자 생성
+        //순환 참조 방지를 위해 weak self 처리
+        Auth.auth().createUser(withEmail: email, password: password){[weak self] authResult, error in
+            //회원가입이 끝났거나 취소되었을 때 호출되는 클로저
+            guard let self = self else {return}
+            
+            if let error = error {//오류가 발생한 경우
+                let code = (error as NSError).code
+                switch code {
+                case 17007: //이미 가입한 계정인 경우
+                    self.signInUer(withEmail: email, password: password) //로그인 하기
+                default:
+                    self.errorMessageLabel.text = error.localizedDescription
+                }
+            }else {//정상적으로 회원가입이 된 경우
+                self.showMainViewController()
+            }
+        }
+    }
+    
+    private func showMainViewController(){
+        let main = UIStoryboard(name: "Main", bundle: Bundle.main)
         guard let mainViewController = main.instantiateViewController(withIdentifier: "MainViewController") as? MainViewController else {return}
         
+        mainViewController.modalPresentationStyle = .fullScreen
         self.navigationController?.pushViewController(mainViewController, animated: true)
+    }
+    
+    //Firebase 인증을 통해 로그인할 수 있는 함수
+    private func signInUer(withEmail email: String, password: String){
+        Auth.auth().signIn(withEmail: email, password: password){ [weak self] _, error in
+            guard let self = self else {return}
+            
+            if let error = error {//로그인 에러가 발생한 경우
+                self.errorMessageLabel.text = error.localizedDescription
+            }else{//정상적으로 로그인된 경우
+                self.showMainViewController()
+            }
+            
+        }
     }
 }
 
--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/MainViewController.swift
@@ -6,15 +6,25 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class MainViewController: UIViewController {
     @IBOutlet weak var welcomeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         //navigationBar가 보이거나 popgesture를 했을 때 보이는 뒤로 가기 제스처 비활성화
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let email = Auth.auth().currentUser?.email ?? "고객"
+        
+        welcomeLabel.text = """
+환영합니다.
+\(email)님
+"""
     }
     
     @IBAction func tapSignOutButton(_ sender: UIButton) {
