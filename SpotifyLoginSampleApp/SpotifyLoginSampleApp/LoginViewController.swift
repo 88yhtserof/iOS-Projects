0305a3eca82f54c3860e12b6c8102716202df463
LimYunHwi <88yhtserof@naver.com>
2022-05-25 17:12:23 +0900

    [FEAT] Google 로그인 기능 구현 #1
    + 개인정보 보호를 위해 GoogleService-Info 일부 누락

--- SpotifyLoginSampleApp/SpotifyLoginSampleApp/LoginViewController.swift
+++ SpotifyLoginSampleApp/SpotifyLoginSampleApp/LoginViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
 class LoginViewController: UIViewController {
     @IBOutlet weak var emailSignInButton: UIButton!
@@ -14,7 +16,7 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         [emailSignInButton, googleSignInButton,appleSignInButton].forEach{
             $0?.layer.borderWidth = 1
             $0?.layer.borderColor = UIColor.white.cgColor
@@ -30,8 +32,47 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func tapGoogleSignInButton(_ sender: UIButton) {
+        //구글 로그인  Firebase개발 문서
+        //https://firebase.google.com/docs/auth/ios/google-signin?hl=ko#swift_3
+        
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        
+        // Start the sign in flow!
+        //구글 로그인
+        GIDSignIn.sharedInstance.signIn(with: config, presenting: self) { [unowned self] user, error in
+            
+            if let error = error {
+                print("ERROR: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let authentication = user?.authentication,
+                  let idToken = authentication.idToken
+            else {return}
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken,
+                                                           accessToken: authentication.accessToken)
+            
+            //Firebase에 사용자 정보 등록하기
+            Auth.auth().signIn(with: credential){_, _ in
+                self.showMainViewController()
+            }
+        }
     }
     
     @IBAction func tapAppleSignInButton(_ sender: UIButton) {
+    }
+    
+    //메인 화면으로 이동
+    private func showMainViewController(){
+        let main = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        guard let mainViewController = main.instantiateViewController(withIdentifier: "MainViewController") as? MainViewController else {return}
+        
+        self.navigationController?.pushViewController(mainViewController, animated: true)
+                
     }
 }
