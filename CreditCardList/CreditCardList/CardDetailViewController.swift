9a2f7dad480e89b5c4903d850ba2cb4835e09d9d
LimYunHwi <88yhtserof@naver.com>
2022-06-24 22:40:54 +0900

    [FEAT] 카드 상세 설명 UI 구성 #3

---
+++ CreditCardList/CreditCardList/CardDetailViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CardDetailViewController.swift
+//  CreditCardList
+//
+//  Created by 임윤휘 on 2022/06/24.
+//
+
+import UIKit
+
+class CardDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
