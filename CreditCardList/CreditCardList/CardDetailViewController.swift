30b711eb173e346c24bc701de1a94eeacb2fda92
LimYunHwi <88yhtserof@naver.com>
2022-06-24 22:53:37 +0900

    [FEAT] Lottie 애니메이션 이미지 추가 #3

--- CreditCardList/CreditCardList/CardDetailViewController.swift
+++ CreditCardList/CreditCardList/CardDetailViewController.swift
@@ -6,10 +6,21 @@
 //
 
 import UIKit
+import Lottie
 
 class CardDetailViewController: UIViewController {
-
+    @IBOutlet weak var lottieView: LottieView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //Lottie 라이브러리 사용
+        //움직이는 애니메이션 뷰.
+        let animationView = AnimationView(name: "money")
+        lottieView.contentMode = .scaleAspectFit
+        lottieView.addSubview(animationView)
+        animationView.frame = lottieView.bounds
+        animationView.loopMode = .loop
+        animationView.play()
     }
 }
