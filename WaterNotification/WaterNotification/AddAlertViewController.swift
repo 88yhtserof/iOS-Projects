363d8c43c123eef3fb061427059ca98a1f97de51
LimYunHwi <88yhtserof@naver.com>
2022-07-07 01:52:11 +0900

    [FEAT] 알림 추가 화면 UI 구성 #5

---
+++ WaterNotification/WaterNotification/AddAlertViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddAlertViewController.swift
+//  WaterNotification
+//
+//  Created by 임윤휘 on 2022/07/07.
+//
+
+import UIKit
+
+class AddAlertViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
