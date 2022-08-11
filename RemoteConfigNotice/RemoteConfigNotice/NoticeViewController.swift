761cd165ec37c3f5203ecb18696b78fef4dba333
LimYunHwi <88yhtserof@naver.com>
2022-08-11 15:37:49 +0900

    [FEAT] 데이터 연결 및 '확인' 버튼 동작 구현 #9

--- RemoteConfigNotice/RemoteConfigNotice/NoticeViewController.swift
+++ RemoteConfigNotice/RemoteConfigNotice/NoticeViewController.swift
@@ -8,16 +8,14 @@
 import UIKit
 
 class NoticeViewController: UIViewController {
+    //부모 뷰컨트롤러에서 데이터를 받아올 튜플 선언
+    var noticeContent: (title: String, detail: String, date: String)? //데이터가 없을 수도 있으므로 옵셔널
+    
     @IBOutlet weak var noticeView: UIView!
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -27,5 +25,15 @@ class NoticeViewController: UIViewController {
         //팝업창처럼 보이도록 배경 색상 처리
         //검정색에 알파값을 주어 이전 화면이 보이도록 처리한다.
         view.backgroundColor = UIColor.black.withAlphaComponent(0.5)
+        
+        //받아온 데이터 UI에 연결하기
+        guard let noticeContent = noticeContent else { return }
+        titleLabel.text = noticeContent.title
+        detailLabel.text = noticeContent.detail
+        dateLabel.text = noticeContent.date
+    }
+    
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+        self.dismiss(animated: true)
     }
 }
