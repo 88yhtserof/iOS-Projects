1c4df54dca4fe519d495c5b60ce119ba8bbc1bfc
LimYunHwi <88yhtserof@naver.com>
2022-08-18 14:45:19 +0900

    [FEAT] 네비게이션 바 설정 #11

--- NetflixMovieRecommendation/NetflixMovieRecommendation/HomeViewController.swift
+++ NetflixMovieRecommendation/NetflixMovieRecommendation/HomeViewController.swift
@@ -8,4 +8,16 @@
 import UIKit
 
 class HomeViewController: UICollectionViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //네비게이션 설정
+        navigationController?.navigationBar.backgroundColor = .clear
+        navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        navigationController?.navigationBar.shadowImage = UIImage() //그림자를 설정해 아래 화면과 구분
+        navigationController?.hidesBarsOnSwipe = true //스크롤을 통해 스와이프 액션이 발생하는 경우 네비게이션 바 숨기기
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "netflix_icon"), style: .plain, target: nil, action: nil)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+    }
 }
