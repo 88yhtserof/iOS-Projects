3395e7ea858c662e793edbed78ef8c3f2928101f
LimYunHwi <88yhtserof@naver.com>
2022-08-18 16:23:02 +0900

    [FEAT] 데이터 추가 및 받아오기 구현 #11

---
+++ NetflixMovieRecommendation/NetflixMovieRecommendation/Content.swift
@@ -0,0 +1,31 @@
+//
+//  Content.swift
+//  NetflixMovieRecommendation
+//
+//  Created by 임윤휘 on 2022/08/18.
+//
+
+import UIKit
+
+struct Content: Decodable {
+    let sectiontype: SectionType //몇 가지 타입 형태가 고정되어 있기 때문에 string말고 열거형을 사용하자
+    let sectionName: String
+    let contentItem: [Item]
+    
+    enum SectionType: String, Decodable {
+        case basic
+        case main
+        case large
+        case rank
+    }
+}
+
+struct Item: Decodable {
+    let description: String
+    let imageName: String
+    
+    //포스트명이 받아오는 포스터 key값과 동일하기 때문에 바로 UIImage로 변환하여 내보낼 수 있다.
+    var image: UIImage {
+        return UIImage(named: imageName) ?? UIImage()
+    }
+}
--- NetflixMovieRecommendation/NetflixMovieRecommendation/HomeViewController.swift
+++ NetflixMovieRecommendation/NetflixMovieRecommendation/HomeViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class HomeViewController: UICollectionViewController {
+    var contents: [Content] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,5 +20,20 @@ class HomeViewController: UICollectionViewController {
         
         navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "netflix_icon"), style: .plain, target: nil, action: nil)
         navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "person.crop.circle"), style: .plain, target: nil, action: nil)
+        
+        //Data 설정 및 가져오기
+        contents = getContents()
+    }
+    
+    //컨텐츠 배열이 Content.plist에 있는 값을 가지고 오는 메서드
+    func getContents() -> [Content] {
+        //Content.plist의 경로 알아내기
+        guard let path = Bundle.main.path(forResource: "Content", ofType: "plist"), //Conetent 이름을 가지고 파일 타입은 plist이다
+              data = FileManager.default.contents(atPath: path), //FileManager를 통해서 path 경로에 있는 데이터 가져오기
+              let list = try? PropertyListDecoder().decode([Content], from: data) else { return []}
+        //data로부터 [Content]타입의 프로퍼티리스트 디코드하기
+        
+        
+        
     }
 }
