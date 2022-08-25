17293497a301b251eef9691f88870a9c3fea4e6c
LimYunHwi <88yhtserof@naver.com>
2022-08-25 12:51:03 +0900

    [ADD] 프로젝트 생성 및 설정 #13

---
+++ BreweryIntroductionService/BreweryIntroductionService/BeerListTableViewController.swift
@@ -0,0 +1,11 @@
+//
+//  BeerListTableViewController.swift
+//  BreweryIntroductionService
+//
+//  Created by 임윤휘 on 2022/08/25.
+//
+
+import UIKit
+
+class BeerListTableViewController: UITableViewController {
+}
