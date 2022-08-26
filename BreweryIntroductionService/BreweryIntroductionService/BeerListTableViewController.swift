0872ee05c474fe3c6708660181d2b80875c52f45
LimYunHwi <88yhtserof@naver.com>
2022-08-26 13:53:52 +0900

    [FEAT] BeerList 네비게이션 바 UI 설정 #13

--- BreweryIntroductionService/BreweryIntroductionService/BeerListTableViewController.swift
+++ BreweryIntroductionService/BreweryIntroductionService/BeerListTableViewController.swift
@@ -8,4 +8,15 @@
 import UIKit
 
 class BeerListTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Configure
+    private func configureNavigationBar() {
+        title = "브루어리"
+        navigationController?.navigationBar.prefersLargeTitles = true //네비게이션바의 제목을 큰 스타일로 보여주기
+        
+    }
 }
