02948f02125145441219947f5a7ce48aac63aa3d
LimYunHwi <88yhtserof@naver.com>
2022-08-30 15:28:47 +0900

    [ADD] 프로젝트 생성 및 설정 #15

---
+++ AppStoreStyleService/AppStoreStyleService/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AppStoreStyleService
+//
+//  Created by 임윤휘 on 2022/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
