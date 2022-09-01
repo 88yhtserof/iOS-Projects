5d720d197c6749e965677c1c0174f25a35674b4a
LimYunHwi <88yhtserof@naver.com>
2022-09-01 20:35:32 +0900

    [FEAT] App 상세 화면: UI 구성 및 화면 전환 연결 #15

--- AppStoreStyleService/AppStoreStyleService/Scenes/Today/TodayViewController.swift
+++ AppStoreStyleService/AppStoreStyleService/Scenes/Today/TodayViewController.swift
@@ -76,4 +76,10 @@ extension TodayViewController: UICollectionViewDelegateFlowLayout {
         
         return UIEdgeInsets(top: value, left: value, bottom: value, right: value)
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let appDetailViewController = AppDetailViewController()
+        
+        self.present(appDetailViewController, animated: true)
+    }
 }
