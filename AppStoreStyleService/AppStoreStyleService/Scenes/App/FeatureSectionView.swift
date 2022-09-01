a452198c42c47188cad638ad1e4b3426c3015d91
LimYunHwi <88yhtserof@naver.com>
2022-09-01 15:18:31 +0900

    [FEAT] App 화면: FeatureSectionView 및 collectionView과 cell 구성 #15

---
+++ AppStoreStyleService/AppStoreStyleService/Scenes/App/FeatureSectionView.swift
@@ -0,0 +1,84 @@
+//
+//  FeatureSectionView.swift
+//  AppStoreStyleService
+//
+//  Created by 임윤휘 on 2022/09/01.
+//
+
+import SnapKit
+import UIKit
+
+final class FeatureSectionView: UIView {
+    private lazy var collectionView: UICollectionView = {
+       let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        collectionView.isPagingEnabled = true //collectionView을 스크롤할 시 한 cell만 화면에 보이도록 설정
+        collectionView.backgroundColor = .systemBackground
+        collectionView.showsHorizontalScrollIndicator =  false
+        
+        //임시 등록
+        collectionView.register(FeatureSectionCollectionViewCell.self, forCellWithReuseIdentifier: "FeatureSectionCollectionViewCell")
+        
+        return collectionView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension FeatureSectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeatureSectionCollectionViewCell", for: indexPath) as? FeatureSectionCollectionViewCell else { return UICollectionViewCell() }
+        
+        cell.setUp()
+        
+        return cell
+    }
+}
+
+
+extension FeatureSectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //좌우 16 마진이 있으므로 -32
+        CGSize(width: collectionView.frame.width - 32.0 , height: self.frame.width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        
+        //이 값을 지정해 주지 않으면, cell이 중앙정렬되지 않는다.
+        return UIEdgeInsets(top: 0.0, left: 16.0, bottom: 0.0, right: 16.0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        32.0
+    }
+}
+
+private extension FeatureSectionView {
+    func setUpViews() {
+        [
+            collectionView
+        ].forEach{ addSubview($0) }
+        
+        collectionView.snp.makeConstraints {
+            $0.leading.trailing.bottom.equalToSuperview()
+            $0.top.equalToSuperview().inset(16.0)
+            $0.height.equalTo(self.snp.width)
+        }
+    }
+}
