ce77496e61a20ff635e3671f3303217743f0d23e
LimYunHwi <88yhtserof@naver.com>
2022-11-29 13:54:30 +0900

    [FEAT] 지하철역 TableView 구성 #17
    - cell.defaultContentConfiguaration() 사용

--- SubwayArrivalInfo/SubwayArrivalInfo/StationSearchViewController.swift
+++ SubwayArrivalInfo/SubwayArrivalInfo/StationSearchViewController.swift
@@ -9,6 +9,15 @@ import SnapKit
 import UIKit
 
 class StationSearchViewController: UIViewController {
+    private var numberOfStation = 0
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.isHidden = true
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,8 +28,55 @@ class StationSearchViewController: UIViewController {
         let searchController = UISearchController()
         searchController.searchBar.placeholder = "지하철 역을 입력해주세요."
         searchController.obscuresBackgroundDuringPresentation = false //검색하는 동안 화면을 어둡게 할지 말지 설정
+        searchController.searchBar.delegate = self
         
         navigationItem.searchController = searchController
+        
+        ConfigureView()
     }
+    
+    private func ConfigureView() {
+        view.addSubview(tableView)
+        tableView.snp.makeConstraints{ make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
+
+extension StationSearchViewController: UISearchBarDelegate {
+    //사용자가 편집을 시작했을 때 호출
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        numberOfStation = 10
+        tableView.isHidden = false
+        tableView.reloadData()
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        numberOfStation = 0
+        tableView.isHidden = true
+        tableView.reloadData()
+    }
+}
+
+extension StationSearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return numberOfStation
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = "\(indexPath.row + 1)"
+        content.secondaryText = "\(indexPath.row + 1)번 cell"
+        content.image = .init(systemName: "person.fill")
+        content.imageProperties.tintColor = .darkGray
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    
 }
 
