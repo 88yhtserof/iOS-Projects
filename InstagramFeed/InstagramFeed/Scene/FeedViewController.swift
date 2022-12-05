fa7abf321f74db40346d0ab1a1159706c5294d11
LimYunHwi <88yhtserof@naver.com>
2022-12-05 11:52:20 +0900

    [FEAT] Feed: TableView 기본 구성#19

--- InstagramFeed/InstagramFeed/Scene/FeedViewController.swift
+++ InstagramFeed/InstagramFeed/Scene/FeedViewController.swift
@@ -9,11 +9,36 @@ import SnapKit
 import UIKit
 
 class FeedViewController: UIViewController {
+    private lazy var tableView: UITableView = {
+        var tableView = UITableView(frame: .zero)
+        tableView.backgroundColor = .systemBackground
+        //tableView를 CollectionView처럼 활용할 것이기 떄문에
+        tableView.separatorStyle = .none//구분선 삭제
+        
+        tableView.dataSource = self
+        
+        return tableView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationBar()
+        configureTableView()
+    }
+}
+
+extension FeedViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.backgroundColor = .darkGray
+        
+        return cell
     }
 }
 
@@ -28,5 +53,13 @@ private extension FeedViewController {
             action: nil
         )
         navigationItem.rightBarButtonItem = uploadButton
+    }
+    
+    func configureTableView() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
     }
 }
