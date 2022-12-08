d72ea3f8ae1829a7defc998068b96bb8f42fe33c
LimYunHwi <88yhtserof@naver.com>
2022-12-08 19:28:11 +0900

    [FEAT] Write: Navigation Bar 구현 #19

--- InstagramFeed/InstagramFeed/Scene/Write/UploadViewController.swift
+++ InstagramFeed/InstagramFeed/Scene/Write/UploadViewController.swift
@@ -8,9 +8,45 @@
 import UIKit
 
 class UploadViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        configureNavigationBar()
+        configureView()
+    }
+}
+
+private extension UploadViewController {
+    func configureNavigationBar(){
+        navigationItem.title = "새 게시물"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            title: "취소",
+            style: .plain,
+            target: self,
+            action: #selector(didTapLeftButton)
+        )
+        navigationItem.rightBarButtonItem = UIBarButtonItem(
+            title: "공유",
+            style: .plain,
+            target: self,
+            action: #selector(didTapRightButton)
+        )
+    }
+    
+    @objc func didTapLeftButton(){
+        print("Did Tap LeftButton")
         
+        dismiss(animated: true)
+    }
+    
+    @objc func didTapRightButton(){
+        print("Did Tap RightButton")
+        
+        dismiss(animated: true)
+    }
+    
+    func configureView(){
         view.backgroundColor = .systemBackground
     }
 }
