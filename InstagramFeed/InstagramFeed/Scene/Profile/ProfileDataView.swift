fb9ae0eb278bc1e56607ed194cadcebfe4427510
LimYunHwi <88yhtserof@naver.com>
2022-12-08 12:47:34 +0900

    [FEAT] Profile: 팔로우 DataView class 생성 #19

---
+++ InstagramFeed/InstagramFeed/Scene/Profile/ProfileDataView.swift
@@ -0,0 +1,58 @@
+//
+//  ProfileDataView.swift
+//  InstagramFeed
+//
+//  Created by 임윤휘 on 2022/12/08.
+//
+
+import SnapKit
+import UIKit
+
+final class ProfileDataView: UIView {
+    private let title: String
+    private let count: String
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16.0, weight: .bold)
+        label.text = "팔로워"
+        
+        return label
+    }()
+    
+    private lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 16.0, weight: .bold)
+        label.text = "100"
+        
+        return label
+    }()
+    
+    init(title: String, count: String) {
+        self.title = title
+        self.count = count
+        
+        super.init(frame: .zero)
+        
+        configureView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension ProfileDataView {
+    func configureView() {
+        let stackView = UIStackView(arrangedSubviews: [countLabel, titleLabel])
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 4.0
+        
+        addSubview(stackView)
+        
+        stackView.snp.makeConstraints{ make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
