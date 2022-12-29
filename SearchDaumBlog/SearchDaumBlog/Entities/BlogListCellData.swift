5f11d4da58a0eeccba0870bf38059f7cd5b85b21
LimYunhwi <88yhtserof@naver.com>
2022-12-29 14:58:25 +0900

    [FEAT] 블로그 리스트 데이터 모델 생성 #23

---
+++ SearchDaumBlog/SearchDaumBlog/Entities/BlogListCellData.swift
@@ -0,0 +1,15 @@
+//
+//  BlogListCellData.swift
+//  SearchDaumBlog
+//
+//  Created by 임윤휘 on 2022/12/29.
+//
+
+import Foundation
+
+struct BlogListCellData {
+    let thumbnailURL: URL?
+    let name: String?
+    let title: String?
+    let datetime: Date?
+}
