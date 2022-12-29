861868297d84a17d50afbb7632a646c26d1ad73a
LimYunhwi <88yhtserof@naver.com>
2022-12-29 16:22:06 +0900

    [FEAT] Main 화면 View 배치 #23

--- SearchDaumBlog/SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
+++ SearchDaumBlog/SearchDaumBlog/Presentation/MainViewController/MainViewController.swift
@@ -9,12 +9,44 @@ import RxSwift
 import RxCocoa
 
 class MainViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    let disposeBag = DisposeBag()
+    
+    let searchBar = SearchBar()
+    let listView = BlogListUITableView()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        bind()
+        attribute()
+        layout()
     }
-
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func bind() {
+    }
+    
+    private func attribute() {
+        title = "다음 블로그 검색"
+        view.backgroundColor = .white
+    }
+    
+    private func layout() {
+        [ searchBar, listView ]
+            .forEach { view.addSubview($0) }
+        
+        searchBar.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        listView.snp.makeConstraints {
+            $0.top.equalTo(searchBar.snp.bottom)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
+    }
 }
 
