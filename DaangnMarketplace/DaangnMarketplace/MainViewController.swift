a2cb95a13d686f9f242920a69d97298a5fa0adbd
88yhtserof <88yhtserofg@gmail.com>
2023-03-31 15:39:33 +0900

    [FEAT] MainView: UI 구성 #27

--- DaangnMarketplace/DaangnMarketplace/MainViewController.swift
+++ DaangnMarketplace/DaangnMarketplace/MainViewController.swift
@@ -8,7 +8,48 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
 
 class MainViewController: UIViewController {
+    let disposeBag = DisposeBag()
     
+    let tableView = UITableView()
+    let submitButton = UIBarButtonItem()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        attribute()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func bind(_ viewModel: MainViewModel) {
+        
+    }
+    
+    private func attribute() {
+        title = "중고거래 글쓰기"
+        view.backgroundColor = .white
+        
+        submitButton.title = "제출"
+        submitButton.style = .done
+        
+        navigationItem.setRightBarButton(submitButton, animated: true)
+        
+        tableView.backgroundColor = .white
+        tableView.separatorStyle = .singleLine
+        tableView.tableFooterView = UIView() //tableView 하단에 cell이 없으면 footerView가 보여 구분선이 보이지 않도록
+    }
+    
+    func layout() {
+        view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
 }
