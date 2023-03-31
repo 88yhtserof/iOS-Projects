3f250ece57f0c228ffe752ad215c1eff5a93e78c
88yhtserof <88yhtserofg@gmail.com>
2023-03-31 15:31:43 +0900

    [FEAT] 프로젝트 초기 설정 #27

---
+++ DaangnMarketplace/DaangnMarketplace/MainViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewController.swift
+//  DaangnMarketplace
+//
+//  Created by 임윤휘 on 2023/03/31.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class MainViewController: UIViewController {
+    
+}
---
+++ DaangnMarketplace/DaangnMarketplace/MainViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  MainViewModel.swift
+//  DaangnMarketplace
+//
+//  Created by 임윤휘 on 2023/03/31.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+struct MainViewModel {
+    
+}
